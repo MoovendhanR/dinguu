dd43539b08f75b3f963812417776be62ee301065
MoovendhanR <moovendhanr1011@gmail.com>
2024-03-14 19:51:45 +0530

    project initialized first problem updated

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Center } from '@chakra-ui/react';
+import Number12 from './Components/Number12';
+
+function App() {
+  return (
+    <Center>
+      <Number12/>
+    </Center>
+  );
+}
+
+export default App;
---
+++ src/Components/Number12.jsx
@@ -0,0 +1,28 @@
+import { Box, Button, Text } from '@chakra-ui/react'
+import React, { useState } from 'react'
+
+const Number12 = () => {
+   const [count,setCount]=useState(0)
+
+// it will take all previous values
+   const handleClick=()=>{
+    setCount((prev)=>prev+1)
+    setCount((prev)=>prev+1)
+    setCount((prev)=>prev+1) 
+}
+// it will update only once
+    // const handleClick=()=>{
+    //        setCount(count+1)
+    //        setCount(count+1)
+    //        setCount(count+1)
+    // }
+
+  return (
+    <Box align='center' mt="20%">
+        <Button colorScheme='blue' onClick={handleClick} >Button</Button>
+          <Text size="xxl" color={"red"}>{count}</Text>
+    </Box>
+  )
+}
+
+export default Number12
